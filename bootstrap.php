743e248996564a33221ae9d7840de40bb1ece42d
Alper\AlperWork <alkilicalp@gmail.com>
2017-12-30 20:10:27 +0300

    2
    test2

--- bootstrap.php
+++ bootstrap.php
@@ -3,28 +3,23 @@
 use Flarum\Event\PostWillBeSaved;
 use Illuminate\Contracts\Events\Dispatcher;
 
-<?php
-  require_once(dirname( __FILE__ ) . "/ChatweeV2_SDK/Chatwee.php");
->
 
-<?php
+  require_once(dirname( __FILE__ ) . "/ChatweeV2_SDK/Chatwee.php");
+
+
+
   ChatweeV2_Configuration::setChatId(5a3caa9abd616dfa624c484a);
   ChatweeV2_Configuration::setClientKey(846468c658a05ec511fadfab);
->
+
 
 return function (Dispatcher $events) {
+
     $events->listen(PostWillBeSaved::class, function (PostWillBeSaved $event) {
             $event->post->content = 'This is not what I wrote!';
     });
 	
+	
 	$events->listen(UserLoggedIn::class, function (UserLoggedIn $event) {
-            <?php
-
-  require_once(dirname( __FILE__ ) . "/ChatweeV2_SDK/Chatwee.php");
-
-  ChatweeV2_Configuration::setChatId(5a3caa9abd616dfa624c484a);
-  ChatweeV2_Configuration::setClientKey(846468c658a05ec511fadfab);
-
   try {
     $sessionId = ChatweeV2_SsoManager::loginUser(Array(
       "userId" => "507f191e810c19729de860ea"
@@ -33,6 +28,6 @@ return function (Dispatcher $events) {
   } catch(Exception $exception) {
     echo "An error occured: " . $exception->getMessage();
   }
->
+
     });
 };
