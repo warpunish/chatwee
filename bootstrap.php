0fe100c61153baf4a32d2c809f22d0a2cf50e9b7
Çağdaş Öksüztepe <cagdasoks@gmail.com>
2017-12-30 20:43:11 +0300

    aa
    aaaa

--- bootstrap.php
+++ bootstrap.php
@@ -4,12 +4,6 @@ use Flarum\Event\PostWillBeSaved;
 use Illuminate\Contracts\Events\Dispatcher;
 
 
-  require_once(dirname( __FILE__ ) . "/ChatweeV2_SDK/Chatwee.php");
-
-
-
-//  ChatweeV2_Configuration::setChatId(5a3caa9abd616dfa624c484a);
-//  ChatweeV2_Configuration::setClientKey(846468c658a05ec511fadfab);
 
 
 return function (Dispatcher $events) {
@@ -19,15 +13,25 @@ return function (Dispatcher $events) {
     });
 	
 	
-//	$events->listen(UserLoggedIn::class, function (UserLoggedIn $event) {
-//  try {
-//    $sessionId = ChatweeV2_SsoManager::loginUser(Array(
-//      "userId" => "507f191e810c19729de860ea"
-//    ));
-//    echo "The user has been logged in with the following session ID: " . $sessionId;
-//  } catch(Exception $exception) {
-//    echo "An error occured: " . $exception->getMessage();
-//  }
+	$events->listen(UserLoggedIn::class, function (UserLoggedIn $event) {
+		 require_once(dirname( __FILE__ ) . "/ChatweeV2_SDK/Chatwee.php");
+         ChatweeV2_Configuration::setChatId(5a3caa9abd616dfa624c484a);
+         ChatweeV2_Configuration::setClientKey(846468c658a05ec511fadfab);
 
-//    });
+  try {
+
+
+
+
+
+	  
+    $sessionId = ChatweeV2_SsoManager::loginUser(Array(
+      "userId" => "507f191e810c19729de860ea"
+    ));
+    echo "The user has been logged in with the following session ID: " . $sessionId;
+  } catch(Exception $exception) {
+    echo "An error occured: " . $exception->getMessage();
+  }
+
+   });
 };
